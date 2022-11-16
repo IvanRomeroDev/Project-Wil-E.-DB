96aaee53ccf53cde74311a6a232e112407146fdd
Jorge Raúl Tzab López <tzablopezjorgeraul@gmail.com>
2022-11-16 09:45:13 -0600

    test

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const subProcess = require('child_process');
+
+/// compila el programa cuando se inicia el servidor
+/*subProcess.execSync("g++ processor.cpp -o processor", (err, stdout, stderr) => {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    } else {
+      console.log(`The stdout Buffer from shell: ${stdout.toString()}`)
+      console.log(`The stderr Buffer from shell: ${stderr.toString()}`)
+    }
+});*/
 
 const PORT = process.env.PORT || 3000;
 
@@ -11,7 +23,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/', (req, res) => {
+app.post('/respuesta', (req, res) => {
     const user_data = req.body;
 
     /**
@@ -28,12 +40,21 @@ app.post('/', (req, res) => {
     });
 
     for(const prop in user_data){
-        fs.appendFile("data.txt", user_data[prop] + "\n", (err) => {
+        fs.appendFile("data.txt", prop + ' ' + user_data[prop] + "\n", (err) => {
             if(err) throw err;
         });
     }
     
     /// ejecuto el programa en cpp desde la terminal
+    subProcess.execFileSync("processor", (err, stdout, stderr) => {
+        if (err) {
+          console.error(err)
+          process.exit(1)
+        } else {
+          console.log(`The stdout Buffer from shell: ${stdout.toString()}`)
+          console.log(`The stderr Buffer from shell: ${stderr.toString()}`)
+        }
+    });
     
     /// leo la respuesta
     const response = require("./response.json");
@@ -42,7 +63,7 @@ app.post('/', (req, res) => {
     }
 
     /// envio la respuesta al cliente
-    res.send(JSON.stringify(response));
+    res.sendFile(path.join(__dirname, 'public/respuesta.html'));
 });
 
 app.listen(PORT);
--- processor.cpp
+++ processor.cpp
@@ -4,28 +4,19 @@
 * LinkedIn: https://www.linkedin.com/in/jorgetzab/
 */
 
-#include <bits/stdc++.h>
+#include <fstream>
 
 using namespace std;
 
-long long dp[100];
-
-long long fib(int n){
-    if(n < 2) return n;
-
-    if(!dp[n]){
-        dp[n] = fib(n - 1) + fib(n - 2);
-    }
-
-    return dp[n];
-}
-
 int main(){
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+    ofstream output("response.json");
 
-    int n;
-    cin >> n;
-    cout << fib(n);
+    output << "{\"nombre\": \"Jorge\"}";
+
+    ifstream input("data_servers.tsv");
+    string str;
+    input >> str;
+
+    output.close();
+    return 0;
 }
